b83f4666e93e220efec3e2b411241d8759313b64
srezanova <rezanovasasha@gmail.com>
2017-10-08 20:51:20 +0300

    arrays

--- src/vitalik/arrays.js
+++ src/vitalik/arrays.js
@@ -6,9 +6,15 @@
 // const item1 = array2[3]; // get single array item
 // array2[3] = 10; // set array item
 
-const array3 = [10, 20, 30];
+const array3 = [10, 1, 3, 8, 95, 89, 20, 30];
 
 let sum = 0;
+
+for (let i = 0; i < array3.length; i += 1) {
+  if (array3[i] % 10 === 0) {
+    sum += array3[i];
+  }
+}
 
 // for cycle
 // sum = array3[0] + array3[1] + array3[2]

677e047af9d4f21beee7375ef1dee8a14f0f5913
Alexandra Rezanova <rezanovasasha@gmail.com>
2017-10-08 20:38:02 +0300

    add array task

---
+++ src/vitalik/arrays.js
@@ -0,0 +1,16 @@
+// create new array
+// const array = [];
+
+// const array2 = [1, 2, 3, 4];
+// array2.length; // get array lenght
+// const item1 = array2[3]; // get single array item
+// array2[3] = 10; // set array item
+
+const array3 = [10, 20, 30];
+
+let sum = 0;
+
+// for cycle
+// sum = array3[0] + array3[1] + array3[2]
+
+console.log(sum, sum === 60);

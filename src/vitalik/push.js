06b96a81c62d9b724dfbd2650a3d84b491a5c3f7
srezanova <rezanovasasha@gmail.com>
2017-10-08 21:10:40 +0300

    array push

--- src/vitalik/push.js
+++ src/vitalik/push.js
@@ -3,3 +3,13 @@
 // array.push('hello');
 // array.push('world');
 // array.push('!');
+function getArrayOf1000() {
+  const array = [];
+  for (let i = 0; i < 1000; i += 1) {
+    if (i % 2 !== 0) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+console.log(getArrayOf1000());
